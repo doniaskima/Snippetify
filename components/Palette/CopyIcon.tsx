ee7338bed0571ccbd317abeb6fe7f24036b4bd04
unknown <doniaskima344@gmail.com>
2023-07-16 00:41:26 +0100

    feat(copy Icon)

--- components/Palette/CopyIcon.tsx
+++ components/Palette/CopyIcon.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { FC } from "react";
 
-const CopyIcon = ({ fill, size }) => {
+interface CopyIconProps {
+  fill: string;
+  size: number;
+}
+
+const CopyIcon: FC<CopyIconProps> = ({ fill, size }) => {
   return (
     <svg
       width={`${size}px`}
@@ -11,7 +16,7 @@ const CopyIcon = ({ fill, size }) => {
     >
       <g data-name="Layer 2">
         <g data-name="copy">
-          <rect width={`${size}px`} height={`${size}px`} opacity="0" />
+          <rect width={`${size}px`} height={`${size}px`} opacity="1" />
           <path d="M18 21h-6a3 3 0 0 1-3-3v-6a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3zm-6-10a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1z" />
           <path d="M9.73 15H5.67A2.68 2.68 0 0 1 3 12.33V5.67A2.68 2.68 0 0 1 5.67 3h6.66A2.68 2.68 0 0 1 15 5.67V9.4h-2V5.67a.67.67 0 0 0-.67-.67H5.67a.67.67 0 0 0-.67.67v6.66a.67.67 0 0 0 .67.67h4.06z" />
         </g>
