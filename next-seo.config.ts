4396e0c02199a10c751ee7eeab44cc6111f68185
unknown <doniaskima344@gmail.com>
2023-07-16 10:29:35 +0100

    feat(Optomize SEO)

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,20 +1,21 @@
-const title = "Donia Skima";
-const description = "Student, tech enthusiast ";
+const title = "Snippetify - Code Snippets and Color Generator";
+const description = "Get code snippets and generate colors with Snippetify, the ultimate resource for developers and designers.";
+const canonical = "https://www.snippetify.com";
 
 const SEO = {
   title,
   description,
-  canonical: "",
+  canonical,
   openGraph: {
     type: "website",
     locale: "en_IE",
-    url: "",
+    url: canonical,
     title,
     description,
     images: [
       {
-        url: "",
-        alt: title,
+        url: "https://www.snippetify.com/images/your-image.jpg",
+        alt: "Snippetify - Code Snippets and Color Generator",
         width: 2240,
         height: 1260,
       },
